accd339546a9ae49d86d0ef5bd120a3d0d67cac0
Deepak-Maan <jattdevta1008@gmail.com>
2024-07-08 16:57:50 +0530

    merge code from portfolio

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -1,7 +1,3 @@
 export const H2 = ({ heading, className }) => {
-<<<<<<< HEAD
     return <h2 className={`font-bold text-lightBlack text-4xl1 leading-110 after:absolute relative after:h-[5px] after:w-[64px] after:bg-orange after:bottom-[-20px] after:left-0  ${className}`}>{heading} <span></span></h2>
-=======
-    return <h2 className={`font-bold text-lightBlack text-4xl1 leading-110  ${className}`}>{heading}</h2>
->>>>>>> bc5411b87944537c1e17e8bcb6e9e18aed8beea3
 }

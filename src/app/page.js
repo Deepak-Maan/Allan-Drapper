110abaaf0cd86c7389349af963cec8e6458fd0a3
sanjuverma373 <sv0240866@gmail.com>
2024-07-11 15:52:55 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import Choose from "@/components/coaching/Choose";
 import Gain from "@/components/coaching/Gain";
 import Program from "@/components/coaching/Program";
 import Wellsay from "@/components/coaching/Wellsay";
+import Footer from "@/components/common/Footer";
+import GrabPdf from "@/components/common/GrabPdf";
 import Business from "@/components/several/Business";
 import Related from "@/components/several/Related";
 
--- src/components/several/Related.jsx
+++ src/components/several/Related.jsx
@@ -21,8 +21,8 @@ const Related = () => {
         </div>
         <Swiper
           modules={[Pagination, Autoplay]}
-          spaceBetween={30}
-          slidesPerView={1}
+          spaceBetween={24}
+          slidesPerView={3}
           autoplay={{
             delay: 3000,
             disableOnInteraction: false,
@@ -43,12 +43,14 @@ const Related = () => {
         >
           {relatedSliderList.map((obj, index) => (
             <SwiperSlide key={index}>
-              <div className='border border-offGray bg-white lg:max-w-[364px] w-full'>
-                <Image src={obj.image} alt={obj.title} />
-                <div className='p-4'>
-                  <h4 className='text-lightBlack font-bold text-2xl md:text-2xl1 leading-120 mb-2'>{obj.title}</h4>
-                  <p className='opacity-80 max-w-[503px] w-full mb-3 text-lightBlack font-medium text-lg sm:text-xl md:text-2xl leading-130'>{obj.description}</p>
-                  <a className='font-medium text-lg leading-130 text-skuBlue underline' href={obj.link}>Read more</a>
+              <div className='flex items-center justify-center'>
+                <div className='border border-offGray bg-white lg:max-w-[364px] w-full'>
+                  <Image src={obj.image} alt={obj.title} />
+                  <div className='p-4'>
+                    <h4 className='text-lightBlack font-bold text-2xl md:text-2xl1 leading-120 mb-2'>{obj.title}</h4>
+                    <p className='opacity-80 max-w-[503px] w-full mb-3 text-lightBlack font-medium text-lg sm:text-xl md:text-2xl leading-130'>{obj.description}</p>
+                    <a className='font-medium text-lg leading-130 text-skuBlue underline' href={obj.link}>Read more</a>
+                  </div>
                 </div>
               </div>
             </SwiperSlide>

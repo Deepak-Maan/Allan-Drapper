c70754b5189b045b65f2ae2e2ddf604f82e36a4e
prince-372 <pmor18314@gmail.com>
2024-07-12 10:25:15 +0530

    manage spacing

--- src/components/Portfolio/Companies.jsx
+++ src/components/Portfolio/Companies.jsx
@@ -6,7 +6,7 @@ import { images } from '../common/Helper';
 
 const Companies = () => {
   return (
-    <div className='max-w-[1220px] mx-auto px-3 sm:pb-16 md:pt-[74px] md:mt-[76px] mt-0 sm:pt-[64px] pt-[48px] mb-2'>
+    <div className='max-w-[1220px] mx-auto px-3 md:pt-[74px] md:mt-[76px] mt-0 sm:pt-[64px] pt-[48px]'>
       <H2 className="text-center after:left-1/2 after:-translate-x-1/2 after:!bg-offBlue" heading="Companies Allan Founded" />
       <div className="flex flex-wrap xl:gap-16 gap-10  justify-center sm:pt-[110px] pt-8">
         {images.slice(0, 5).map((image, index) => (
@@ -18,7 +18,7 @@ const Companies = () => {
           <Image className=' duration-300 hover:scale-90' key={index} src={image.src} width={image.width} height={image.height} alt={image.alt} />
         ))}
       </div>
-      <div className='flex justify-center sm:pt-20 pt-4 pb-16 mb-1'>
+      <div className='flex justify-center sm:pt-20 pt-4 mb-1'>
         <Image className=' duration-300 hover:scale-90' src={images[10].src} width={images[10].width} height={images[10].height} alt={images[10].alt} />
       </div>
     </div>
--- src/components/Portfolio/Investments.jsx
+++ src/components/Portfolio/Investments.jsx
@@ -7,9 +7,9 @@ import sky from '../../../public/assets/images/svg/portfolio/skyBlue.svg'
 
 const Investments = () => {
   return (
-    <div className=' max-w-[980px] mx-auto px-3   md:pb-[149px] sm:pb-[80px] pb-10'>
+    <div className=' max-w-[980px] mx-auto px-3 md:pt-[155px] sm:pt-[80px] pt-10 md:pb-[149px] sm:pb-[80px] pb-10'>
       <H2 className="text-center after:left-1/2 after:-translate-x-1/2" heading="Allan’s Investments"/>
-      <div className=' flex flex-wrap sm:gap-20 gap-4 justify-center items-center pt-20'>
+      <div className=' flex flex-wrap sm:gap-20 gap-4 justify-center items-center sm:pt-20 pt-9'>
         <Image className=' duration-300 hover:scale-90' src={slick} width={256} height={103} alt='slick'/>
         <Image className='lg:py-16 md:py-10 py-2 duration-300 hover:scale-90' src={paramify} width={230} height={40} alt='slick'/>
         <Image className='lg:py-12 md:py-8 py-2 duration-300 hover:scale-90' src={sky} width={230} height={75} alt='slick'/>
